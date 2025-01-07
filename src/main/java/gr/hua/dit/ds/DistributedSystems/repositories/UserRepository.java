78025c3902166b2138a73725e557c83fbe629cec
kwotaq <PJXORISJ@gmail.com>
2025-01-07 20:17:13 +0200

    add functions for search

--- src/main/java/gr/hua/dit/ds/DistributedSystems/repositories/UserRepository.java
+++ src/main/java/gr/hua/dit/ds/DistributedSystems/repositories/UserRepository.java
@@ -8,4 +8,11 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
