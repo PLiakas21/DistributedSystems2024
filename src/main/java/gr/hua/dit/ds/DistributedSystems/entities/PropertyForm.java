8be736b5ab884fa925b141643e1e0f4916d2bc97
kwotaq <PJXORISJ@gmail.com>
2025-02-06 21:09:09 +0200

    remove unnecessary address attribute in PropertyForm.java

--- src/main/java/gr/hua/dit/ds/DistributedSystems/entities/PropertyForm.java
+++ src/main/java/gr/hua/dit/ds/DistributedSystems/entities/PropertyForm.java
@@ -7,9 +7,6 @@ import java.util.List;
 public class PropertyForm extends Form {
 
     @Column(nullable = false)
-    private String propertyAddress;
-
-    @Column(nullable = false)
     private double rentPrice;
 
     @Column(nullable = false)
@@ -18,23 +15,14 @@ public class PropertyForm extends Form {
     @OneToMany(cascade= {CascadeType.ALL})
     private List<RentalForm> rentalFormList;
 
-    public PropertyForm(Integer id, User user, String date, String address, String propertyAddress, double rentPrice, boolean openForRenting) {
+    public PropertyForm(Integer id, User user, String date, String address, double rentPrice, boolean openForRenting) {
         super(id, user, date, address);
-        this.propertyAddress = propertyAddress;
         this.rentPrice = rentPrice;
         this.openForRenting = openForRenting;
     }
 
     public PropertyForm() {
         super();
-    }
-
-    public String getPropertyAddress() {
-        return propertyAddress;
-    }
-
-    public void setPropertyAddress(String propertyAddress) {
-        this.propertyAddress = propertyAddress;
     }
 
     public double getRentPrice() {
