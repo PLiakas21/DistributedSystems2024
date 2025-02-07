ea80cb2042aee54d048400248b6c29aa6eaa7bbe
kwotaq <PJXORISJ@gmail.com>
2025-02-07 23:29:40 +0200

    fix constructor

--- src/main/java/gr/hua/dit/ds/DistributedSystems/entities/RentalForm.java
+++ src/main/java/gr/hua/dit/ds/DistributedSystems/entities/RentalForm.java
@@ -5,8 +5,8 @@ import jakarta.persistence.*;
 @Entity
 public class RentalForm extends Form {
 
-    public RentalForm(Integer id, User user, String date, String address) {
-        super(id, user, date, address);
+    public RentalForm(User user, String date, String address) {
+        super(user, date, address);
     }
 
     public RentalForm() {
